4f88b316ca04f94e98364ae22121de9a3a3e60dc
Felipe Milani <felmilani@gmail.com>
2020-08-06 23:37:26 -0300

    Cleanup do comando banco

--- src/commands/banco.ts
+++ src/commands/banco.ts
@@ -1,20 +1,9 @@
 import { Command, flags } from '@oclif/command'
-import fetch from 'node-fetch'
 import getConfig from '../config'
 import { balance } from '../api'
 
 export default class Banco extends Command {
-  static description = 'describe the command here'
-
-  static flags = {
-    help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
-  }
-
-  static args = [{ name: 'file' }]
+  static description = 'Informa o seu banco de horas'
 
   async run() {
     const { args, flags } = this.parse(Banco)
@@ -27,6 +16,7 @@ export default class Banco extends Command {
     const hours = Math.floor(abs_balance / 60 / 60)
     const hours_padded = hours < 10 ? `0${hours}` : hours
     const minutes = (abs_balance / 60) % 60
+
     this.log(`${signal}${hours_padded}:${minutes}`)
   }
 }
